65ed770f605c1ba362ab711ee201852ab5408cca
光岡 高宏/MITSUOKA Takahiro <61106530+mitsuoka0423@users.noreply.github.com>
2025-09-13 05:52:56 +0000

    feat: Add new client per request execution pattern to index.php

--- index.php
+++ index.php
@@ -4,7 +4,24 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/src/helpers.php';
 
 use App\HttpClient;
+use App\Logger;
+use App\LogMiddleware;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Promise\Utils;
+
+// Measure New Client Each Request Execution
+measure_execution_time('New Client Each Request Execution', function () {
+    $promises = [];
+    for ($i = 1; $i <= 25; $i++) {
+        $stack = HandlerStack::create();
+        $stack->push(LogMiddleware::create(new Logger()));
+        $client = new Client(['handler' => $stack]);
+
+        $promises[] = $client->getAsync("https://jsonplaceholder.typicode.com/posts/{$i}");
+    }
+    Utils::all($promises)->wait();
+});
 
 $client = HttpClient::getInstance();
 
@@ -14,6 +31,8 @@ measure_execution_time('Sequential Execution', function () use ($client) {
         $client->getAsync("https://jsonplaceholder.typicode.com/posts/{$i}")->wait();
     }
 });
+
+$client = HttpClient::getInstance();
 
 // Measure Parallel Execution
 measure_execution_time('Parallel Execution', function () use ($client) {
